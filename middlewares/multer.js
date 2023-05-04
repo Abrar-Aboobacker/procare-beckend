c3fa71374b502aa8d3bec725e4455556e5a4e81a
abrar aboobacker <abraraboobackerkk@gmail.com>
2023-05-04 14:49:20 +0530

    bug checking

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -5,11 +5,11 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
 
     let uploadPath = '';
-    if(file.fieldname === 'file'){
-      uploadPath = path.join('public','uploads', 'certificate');
-    }else{
+    // if(file.fieldname === 'file'){
+      // uploadPath = path.join('public','uploads', 'certificate');
+    // }else{
       uploadPath = path.join('public','uploads', 'profile');
-    }
+    // }
     cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
